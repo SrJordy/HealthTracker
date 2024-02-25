a82cb47fc6fca69da435c1c29ec3de3b28cc9167
SrJordy <120031341+SrJordy@users.noreply.github.com>
2024-02-25 09:59:07 -0500

    Update CalendarScreen.tsx

--- Layouts/CalendarScreen.tsx
+++ Layouts/CalendarScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -11,20 +11,56 @@ import {
 } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import axios from 'axios'; // Asegúrate de importar axios
+import axios from 'axios';
 import { useAuth } from './AuthContext';
 import { useNavigation } from '@react-navigation/native';
 
 const CalendarScreen = () => {
   const { user, pacie } = useAuth();
+  console.log('datos del paciente:', pacie.User.ID)
   const navigation = useNavigation();
   const [showEventCreator, setShowEventCreator] = useState(false);
   const [selectedDate, setSelectedDate] = useState('');
   const [events, setEvents] = useState({});
+  const [filteredEvents, setFilteredEvents] = useState([]);
   const [currentEvent, setCurrentEvent] = useState({ date: '', title: '', subject: '', time: new Date() });
   const [showTimePicker, setShowTimePicker] = useState(false);
 
-  console.log('paciente que eve:', pacie.ID);
+  
+  useEffect(() => {
+    const loadEvents = async () => {
+      try {
+        const response = await axios.get('https://carinosaapi.onrender.com/agenda/getAll');
+        const data = response.data;
+        const eventsForPacie = data.filter(event => event.paciente_id === pacie.ID);
+        setFilteredEvents(eventsForPacie);
+      } catch (error) {
+        console.error('Error al obtener los eventos:', error);
+        Alert.alert("Error", "No se pudieron cargar los eventos.");
+      }
+    };
+  
+    const loadPacienteCuidadorRelation = async () => {
+      try {
+        const response = await axios.get('https://carinosaapi.onrender.com/pacientecuidador/getAll');
+        const data = response.data;
+        const userRelation = data.find(relation => relation.Paciente.User.ID === user.ID);
+        if (userRelation) {
+          console.log("Relación Paciente-Cuidador encontrada:", userRelation);
+        } else {
+          console.log("No se encontró una relación Paciente-Cuidador para el usuario.");
+        }
+      } catch (error) {
+        console.error('Error al obtener la relación Paciente-Cuidador:', error);
+      }
+    };
+  
+    loadEvents();
+  
+    if (user.roles.includes('paciente')) {
+      loadPacienteCuidadorRelation();
+    }
+  }, [pacie.ID, user.ID, user.roles]); 
 
   const openEventCreator = () => {
     setShowEventCreator(true);
@@ -51,46 +87,45 @@ const CalendarScreen = () => {
       Alert.alert("Error", "Por favor complete los campos requeridos.");
       return;
     }
-  
+
     const fechaISO = new Date(currentEvent.date + 'T' + currentEvent.time.toISOString().split('T')[1]).toISOString();
-  
+
     const eventToSave = {
       PacienteID: pacie.ID,
       nombre: currentEvent.title,
       descripcion: currentEvent.subject,
       Fecha: fechaISO,
-      Hora: fechaISO  
+      Hora: fechaISO
     };
-  
-    console.log('Datos del evento a guardar:', eventToSave);
-  
+
     axios.post('https://carinosaapi.onrender.com/agenda/insert', eventToSave)
       .then(response => {
         console.log('Evento guardado con éxito:', response.data);
-        // Restablecer currentEvent a sus valores iniciales y cerrar el formulario
         setCurrentEvent({ date: '', title: '', subject: '', time: new Date() });
         setShowEventCreator(false);
-        // Mostrar mensaje de éxito
         Alert.alert("Éxito", "Agenda exitosa");
-        // Aquí podrías también actualizar 'events' si quieres reflejar el nuevo evento en la UI
       })
       .catch(error => {
         console.error('Error al guardar el evento:', error);
         Alert.alert("Error", "No se pudo guardar el evento. Intente nuevamente.");
       });
   };
-  
-  
 
   const handleCancelEvent = () => {
-    setCurrentEvent({ date: '', title: '', subject: '', time: new Date() });
     setShowEventCreator(false);
   };
 
-  const handleDeleteEvent = (eventKey) => {
-    const updatedEvents = { ...events };
-    delete updatedEvents[eventKey];
-    setEvents(updatedEvents);
+  const handleDeleteEvent = (id) => {
+    /*if (user.roles.includes('cuidador')) {
+      axios.delete(`https://carinosaapi.onrender.com/agenda/delete/${id}`)
+        .then(response => {
+          Alert.alert("Éxito", "Evento eliminado exitosamente.");
+        })
+        .catch(error => {
+          console.error('Error al eliminar el evento:', error);
+          Alert.alert("Error", "No se pudo eliminar el evento. Intente nuevamente.");
+        });
+    }*/
   };
 
   return (
@@ -106,7 +141,7 @@ const CalendarScreen = () => {
         return acc;
       }, {})} />
 
-      {user && user.roles.includes('cuidador') && !showEventCreator && (
+      {user && user.roles.includes('cuidador') && selectedDate && (
         <TouchableOpacity style={styles.addButton} onPress={openEventCreator}>
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
@@ -116,7 +151,6 @@ const CalendarScreen = () => {
         <View style={styles.overlay}>
           <View style={styles.eventCreatorContainer}>
             <Text style={styles.fieldLabel}>Fecha: {currentEvent.date}</Text>
-            <Text style={styles.fieldLabel}>Asunto:</Text>
             <TextInput
               style={styles.input}
               placeholder="Escribe el título aquí"
@@ -155,72 +189,82 @@ const CalendarScreen = () => {
       )}
 
       <ScrollView style={styles.eventsListContainer}>
-        {Object.entries(events).map(([key, event]) => (
-          <View key={key} style={styles.eventItem}>
-            <Text style={styles.eventDate}>Fecha: {event.date}</Text>
-            <Text style={styles.eventTitle}>Asunto: {event.title}</Text>
-            <Text style={styles.eventSubject}>Descripción: {event.subject}</Text>
-            <Text style={styles.eventTime}>Hora: {new Date(event.time).toLocaleTimeString()}</Text>
-            {user && user.roles.includes('cuidador') && (
-              <TouchableOpacity style={styles.deleteButton} onPress={() => handleDeleteEvent(key)}>
-                <Text style={styles.deleteButtonText}>Eliminar</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-        ))}
+        {filteredEvents.map((event) => {
+          const fechaFormateada = event.fecha.split('T')[0];
+
+          return (
+            <View key={event.id} style={styles.eventItem}>
+              <Text style={styles.eventDate}>Fecha: {fechaFormateada}</Text>
+              <Text style={styles.eventTitle}>Nombre: {event.nombre}</Text>
+              <Text style={styles.eventSubject}>Descripción: {event.descripcion}</Text>
+              <Text style={styles.eventTime}>Hora: {new Date(event.hora).toLocaleTimeString()}</Text>
+              {user && user.roles.includes('cuidador') && (
+                <TouchableOpacity style={styles.deleteButton} onPress={() => handleDeleteEvent(event.id)}>
+                  <Text style={styles.deleteButtonText}>Eliminar</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          );
+        })}
       </ScrollView>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 10,
+    backgroundColor: '#f0f0f0', // Fondo más claro para el contenedor principal
   },
   backButton: {
-    marginLeft: 10,
-    marginBottom: 10,
+    margin:5,
     padding: 10,
     borderRadius: 5,
-    backgroundColor: '#ddd',
+    backgroundColor: '#007bff', // Botón para volver con el color principal de la app
   },
   backButtonText: {
     fontSize: 18,
-    color: '#000',
+    color: '#ffffff', // Texto blanco para contraste
   },
   overlay: {
     position: 'absolute',
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgba(0,0,0,0.7)', // Hacer el fondo del overlay más oscuro para mejor enfoque
     top: 0,
     bottom: 0,
     left: 0,
     right: 0,
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex:1,
+    zIndex: 1,
   },
   eventCreatorContainer: {
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff', // Fondo blanco para el formulario
     borderRadius: 10,
     width: '90%',
     maxWidth: 400,
+    shadowColor: '#000', // Sombra para darle profundidad
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   fieldLabel: {
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 5,
-    color: 'black',
+    color: '#333333', // Texto oscuro para mejor lectura
   },
   input: {
     marginBottom: 15,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: '#007bff', // Color principal para la línea del input
     padding: 10,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff', // Fondo blanco para los inputs
     borderRadius: 5,
-    color: 'black',
+    color: '#333333', // Texto oscuro para los inputs
   },
   multilineInput: {
     height: 100,
@@ -229,18 +273,18 @@ const styles = StyleSheet.create({
   timeInput: {
     padding: 10,
     marginBottom: 15,
-    backgroundColor: '#eee',
+    backgroundColor: '#eee', // Fondo más claro para el input de tiempo
     borderRadius: 5,
   },
   timeText: {
-    color: 'black',
+    color: '#333333', // Texto oscuro para mejor lectura
   },
   buttonsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
   button: {
-    backgroundColor: '#4CAF50',
+    backgroundColor: '#28a745', // Botón verde para acciones positivas
     padding: 10,
     borderRadius: 5,
     minWidth: 100,
@@ -248,7 +292,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   cancelButton: {
-    backgroundColor: '#f44336',
+    backgroundColor: '#dc3545', // Botón rojo para cancelar o acciones negativas
   },
   buttonText: {
     color: 'white',
@@ -258,15 +302,14 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 20,
     bottom: 20,
-    backgroundColor: '#007bff',
+    backgroundColor: '#17a2b8', // Color distinto para el botón de agregar
     width: 50,
     height: 50,
     borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex: 10, // Asegura que el botón esté por encima de otros elementos
+    zIndex: 10,
   },
-  
   addButtonText: {
     fontSize: 24,
     color: 'white',
@@ -275,35 +318,42 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   eventItem: {
-    backgroundColor: '#f9f9f9',
+    backgroundColor: '#ffffff', // Fondo blanco para los items del evento
     padding: 15,
     borderRadius: 10,
     marginBottom: 10,
     marginHorizontal: 10,
+    shadowColor: '#000', // Sombra para darle profundidad
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   eventDate: {
     fontSize: 16,
-    color: '#333',
+    color: '#333333',
   },
   eventTitle: {
     marginTop: 5,
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#000',
+    color: '#007bff', // Usar el color principal para el título del evento
   },
   eventSubject: {
     fontSize: 16,
-    color: '#666',
-    marginTop: 5,
+    color: '#666666', // Color más suave para la descripción
   },
   eventTime: {
     fontSize: 14,
-    color: '#555',
+    color: '#555555',
     marginTop: 5,
   },
   deleteButton: {
     marginTop: 10,
-    backgroundColor: '#ff6347',
+    backgroundColor: '#dc3545', // Mantener el rojo para eliminar
     padding: 10,
     borderRadius: 5,
   },
@@ -313,6 +363,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
 
 
 
