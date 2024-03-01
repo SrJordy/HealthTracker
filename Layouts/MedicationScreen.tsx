13a845bf8ef9bb7f1975d2850e8e8c1102dda26a
SrJordy <120031341+SrJordy@users.noreply.github.com>
2024-02-29 20:45:25 -0500

    Update MedicationScreen.tsx
    Esta casi al 100 solo falta validar que se filtre de forma correcta los medicamentos y se actualice los medicamentos

--- Layouts/MedicationScreen.tsx
+++ Layouts/MedicationScreen.tsx
@@ -11,14 +11,30 @@ import {
   KeyboardAvoidingView,
   Platform,
   Modal,
+  Alert,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from './AuthContext';
+import { Picker } from '@react-native-picker/picker';
 
 const MedicationReminderScreen = () => {
   const { user, pacie } = useAuth();
+  const pacienteID = user.roles === 'paciente' ? user.Paciente.ID : pacie.ID;
   const navigation = useNavigation();
   const [medicationData, setMedicationData] = useState([]);
+
+  const [medicamentos, setMedicamentos] = useState([]);
+  const [medicamentoSeleccionado, setMedicamentoSeleccionado] = useState(null);
+  const [cantidad, setCantidad] = useState('');
+
+  const [frecuencia, setFrecuencia] = useState('');
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [newMed, setNewMed] = useState({
+    nombre: '',
+    descripcion: '',
+  });
+  const [showOptions, setShowOptions] = useState(false);
+  const [showScheduleForm, setShowScheduleForm] = useState(false);
 
   useEffect(() => {
     let pacienteId = user.roles === 'paciente' ? user.Paciente.ID : pacie.ID;
@@ -55,22 +71,127 @@ const MedicationReminderScreen = () => {
       .catch((error) => console.error('Error al obtener medicamentos:', error));
   }, [user, pacie]);
 
-  const [showAddForm, setShowAddForm] = useState(false);
-  const [newMed, setNewMed] = useState({
-    name: '',
-    dose: '',
-    frequencyHours: '',
-    descripcion: '',
-  });
+  //obtener los medicamentos generales
+  useEffect(() => {
+    const fetchMedicamentos = async () => {
+      try {
+        const response = await fetch('https://carinosaapi.onrender.com/medicamento/getAll');
+        const data = await response.json();
+        setMedicamentos(data);
+      } catch (error) {
+        console.error('Error al obtener los medicamentos:', error);
+        Alert.alert('Error al cargar los medicamentos. Por favor, intente más tarde.');
+      }
+    };
 
-  const handleAddNewMedication = () => {
-    console.log('Agregar medicación:', newMed);
-    setShowAddForm(false);
+    fetchMedicamentos();
+  }, []);
+
+
+  const handleAddNewMedication = async () => {
+    if (!newMed.nombre.trim() || !newMed.descripcion.trim()) {
+      Alert.alert('Por favor, complete todos los campos.');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://carinosaapi.onrender.com/medicamento/insert', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nombre: newMed.nombre,
+          descripcion: newMed.descripcion,
+        }),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        Alert.alert('Medicamento guardado con éxito');
+        setNewMed({ nombre: '', descripcion: '' });
+        setShowAddForm(false);
+        setShowOptions(false);
+      } else {
+        Alert.alert('Error al guardar el medicamento: ' + (result.message || 'Intente nuevamente'));
+      }
+    } catch (error) {
+      console.error('Error al guardar el medicamento:', error);
+      Alert.alert('Error al conectar con el servidor. Por favor, intente más tarde.');
+    }
   };
+
+
 
   const handleCancel = () => {
+    setNewMed({ nombre: '', descripcion: '' }); // Limpiar campos
     setShowAddForm(false);
+    setShowOptions(false);
   };
+
+  const openAddMedicationForm = () => {
+    setShowOptions(false);
+    setShowAddForm(true);
+  };
+
+  const openAddScheduleForm = () => {
+    console.log('Abrir formulario de horarios');
+    setShowOptions(false);
+  };
+
+  const guardarHorario = async () => {
+    console.log("id del paciente",pacienteID);
+    // Asegurarse de que medicamentoSeleccionado no sea null y contenga un id
+    if (!medicamentoSeleccionado || !medicamentoSeleccionado.ID || !cantidad.trim() || !frecuencia.trim()) {
+      Alert.alert('Por favor, complete todos los campos correctamente.');
+      return;
+    }
+  
+    // Asegurarse de que pacienteId esté definido
+    if (!pacienteID) {
+      Alert.alert('Error: El ID del paciente no está disponible.');
+      return;
+    }
+  
+    const dataParaEnviar = {
+      pacienteID: pacienteID,
+      medicamentoID: medicamentoSeleccionado.ID,
+      dosisInicial: parseInt(cantidad, 10),
+      frecuencia: parseInt(frecuencia, 10),
+    };
+  
+    console.log('Datos a guardar:', dataParaEnviar);
+  
+    try {
+      const response = await fetch('https://carinosaapi.onrender.com/horariomedicamentos/insert', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataParaEnviar),
+      });
+  
+      if (response.ok) {
+        Alert.alert('Horario guardado con éxito');
+        setMedicamentoSeleccionado(null);
+        setCantidad('');
+        setFrecuencia('');
+        setShowScheduleForm(false);
+      } else {
+        const errorData = await response.json();
+        Alert.alert('Error al guardar el horario: ' + (errorData.message || 'Por favor, intente nuevamente'));
+      }
+    } catch (error) {
+      console.error('Error al guardar el horario:', error);
+      Alert.alert('Error al conectar con el servidor. Por favor, intente más tarde.');
+    }
+  };
+  
+  
+  
+  
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -96,10 +217,35 @@ const MedicationReminderScreen = () => {
       </ScrollView>
 
       {user.roles === 'cuidador' && (
-        <TouchableOpacity style={styles.addButton} onPress={() => setShowAddForm(true)}>
+        <TouchableOpacity style={styles.addButton} onPress={() => setShowOptions(!showOptions)}>
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
       )}
+
+      {showOptions && (
+        <View style={styles.floatingButtonsContainer}>
+          <TouchableOpacity
+            style={styles.floatingButton}
+            onPress={() => {
+              setShowOptions(false);
+              setShowAddForm(true);
+            }}
+          >
+            <Text style={styles.floatingButtonText}>Medicamentos</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.floatingButton}
+            onPress={() => {
+              setShowOptions(false); // Oculta los botones flotantes
+              setShowScheduleForm(true); // Muestra el formulario de horarios
+            }}
+          >
+            <Text style={styles.floatingButtonText}>Horarios</Text>
+          </TouchableOpacity>
+
+        </View>
+      )}
+
 
       <Modal
         animationType="slide"
@@ -111,27 +257,17 @@ const MedicationReminderScreen = () => {
           <View style={styles.modalView}>
             <Text style={styles.modalTitle}>Agregar Nuevo Medicamento</Text>
 
+            <Text style={styles.fieldTitle}>Nombre del medicamento:</Text>
             <TextInput
               style={styles.input}
-              onChangeText={(text) => setNewMed({ ...newMed, name: text })}
-              value={newMed.name}
+              onChangeText={(text) => setNewMed({ ...newMed, nombre: text })}
+              value={newMed.nombre}
               placeholder="Nombre del medicamento"
             />
+
+            <Text style={styles.fieldTitle}>Descripción:</Text>
             <TextInput
-              style={styles.input}
-              onChangeText={(text) => setNewMed({ ...newMed, dose: text })}
-              value={newMed.dose}
-              placeholder="Dosis"
-            />
-            <TextInput
-              style={styles.input}
-              keyboardType="numeric"
-              onChangeText={(text) => setNewMed({ ...newMed, frequencyHours: text })}
-              value={newMed.frequencyHours}
-              placeholder="Frecuencia (horas)"
-            />
-            <TextInput
-              style={[styles.input, { height: 100, textAlignVertical: 'top' }]}
+              style={[styles.input, styles.inputDescription]}
               onChangeText={(text) => setNewMed({ ...newMed, descripcion: text })}
               value={newMed.descripcion}
               placeholder="Descripción"
@@ -139,8 +275,64 @@ const MedicationReminderScreen = () => {
             />
 
             <View style={styles.buttonGroup}>
-              <Button title="Cancelar" onPress={handleCancel} color="#6c757d" />
-              <Button title="Agregar" onPress={handleAddNewMedication} color="#007bff" />
+              <TouchableOpacity style={styles.buttonCancel} onPress={handleCancel}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonSave} onPress={handleAddNewMedication}>
+                <Text style={styles.buttonText}>Guardar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showScheduleForm}
+        onRequestClose={() => setShowScheduleForm(false)}
+      >
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalTitle}>Configurar Horario</Text>
+
+            <Text style={styles.fieldTitle}>Medicamento:</Text>
+            <Picker
+              selectedValue={medicamentoSeleccionado}
+              onValueChange={(itemValue, itemIndex) => setMedicamentoSeleccionado(medicamentos[itemIndex])}
+              style={styles.input}
+            >
+              {medicamentos.map((medicamento, index) => (
+                <Picker.Item key={index} label={medicamento.nombre} value={medicamento} />
+              ))}
+            </Picker>
+
+            <Text style={styles.fieldTitle}>Cantidad:</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              onChangeText={setCantidad}
+              value={cantidad}
+              placeholder="Cantidad"
+            />
+
+
+            <Text style={styles.fieldTitle}>Frecuencia (horas):</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              onChangeText={setFrecuencia}
+              value={frecuencia}
+              placeholder="Cada cuántas horas"
+            />
+
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity style={styles.buttonCancel} onPress={() => setShowScheduleForm(false)}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonSave} onPress={guardarHorario}>
+                <Text style={styles.buttonText}>Guardar</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </KeyboardAvoidingView>
@@ -153,6 +345,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f8ff', // Curious Blue 50
+  },
+  inputDescription: {
+    height: 100,
+    textAlignVertical: 'top',
+  },
+  floatingButtonsContainer: {
+    position: 'absolute',
+    right: 20,
+    bottom: 90,
+    alignItems: 'flex-end',
+  },
+  floatingButton: {
+    backgroundColor: '#007bff',
+    marginBottom: 10,
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  floatingButtonText: {
+    color: 'white',
+    fontSize: 16,
   },
   staticTitle: {
     fontSize: 22,
@@ -230,26 +443,52 @@ const styles = StyleSheet.create({
   },
   fieldTitle: {
     alignSelf: 'flex-start',
-    margin: 10,
-    fontWeight: 'bold',
     fontSize: 16,
-    color: '#0675c3', // Curious Blue 600
+    fontWeight: 'bold',
+    color: '#086567', // Ajusta el color según tu paleta
+    marginTop: 10,
   },
   input: {
     width: '100%',
-    marginVertical: 10,
+    marginVertical: 8,
     borderWidth: 1,
-    borderColor: '#ced4da', // Neutral color to maintain focus
+    borderColor: '#ced4da',
     borderRadius: 5,
     padding: 10,
     fontSize: 16,
-    backgroundColor: '#eefffc', // Java 50, for a subtle contrast
+    color: '#086567', // Asegúrate de que el color del texto sea visible
+    backgroundColor: '#eefffc', // Ajusta según necesites para contraste
   },
   buttonGroup: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 20,
   },
+  buttonSave: {
+    borderRadius: 20,
+    overflow: 'hidden',
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    width: '48%', // Ajusta el ancho como necesites
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonCancel: {
+    borderRadius: 20,
+    overflow: 'hidden',
+    backgroundColor: '#6c757d',
+    paddingVertical: 10,
+    width: '48%', // Ajusta el ancho como necesites
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+
 });
 
 
